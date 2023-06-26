e06ace7ba8ec4ffcea397ed2f437e72fd005e3aa
Arturo Ortueta <69780116+DevArturoOrtueta@users.noreply.github.com>
2023-06-26 15:47:50 -0500

    26/06/2023

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,15 @@
+import { JournalLayout } from "../layout/JournalLayout"
+import { Typography } from "@mui/material"
+
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, ipsa deleniti fuga distinctio nemo maiores vero nam esse molestias consectetur omnis laboriosam sequi sunt illum ad quae nobis cupiditate debitis.</Typography>
+    
+      {/* NothinSelected */}
+      {/* NoteView */}
+    </JournalLayout>
   )
 }
  
